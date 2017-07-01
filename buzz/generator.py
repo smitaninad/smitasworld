6321bb3fe8e6d25301468e41a999ca2d572434d3
Kelkar <smita.kelkar@siemens.com>
2017-07-01 17:12:16 +0530

    Initial Commit for CI-CD Buzz project

---
+++ buzz/generator.py
@@ -0,0 +1,17 @@
+import random
+
+buzz = ('continuous testing', 'continuous integration',
+    'continuous deployment', 'continuous improvement', 'devops')
+adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
+adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
+    'seriously')
+verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
+def sample(l, n = 1):
+    result = random.sample(l, n)
+    if n == 1:
+        return result[0]
+    return result
+buzz_terms = sample(buzz, 2)
+str= ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),sample(verbs), buzz_terms[1]])
+print (str)
+
